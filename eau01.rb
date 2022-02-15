fa9c64a573f5163849c8226eb6fa6f05fe8e9600
Hugo <h.granier@me.com>
2022-02-15 18:15:48 +0100

    Exo 01 terminer

--- eau01.rb
+++ eau01.rb
@@ -1,19 +1,26 @@
 # Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 00 et 99 dans l’ordre croissant.
 
-def three_figures_combination
-    range = *("000".."999")
-    result = []
-  
-    range.map! do |element|
-      if element.count(element[0]) == 1 && element.count(element[1]) == 1
-        !result.include?(element.chars.sort.join) ? (result.push(element.chars.sort.join)) : ()
-      end
+def compteur(nb1, nb2, nb3, nb4)
+    while nb4 <= 9
+        nb4 +=1
+        if nb4 == 10
+            nb3 +=1
+            nb4 = 0           
+            if nb3 == 10
+                nb2 +=1
+                nb3 = 0
+                nb4 = 0
+                if nb2 == 10
+                    nb1 +=1
+                    nb2 = 0
+                    nb3 = 0
+                    nb4 = 0
+                    break if nb1 == 10
+                end
+            end
+        end
+        print "#{nb1}#{nb2} #{nb3}#{nb4}, "
     end
-    result.join(" ")
-  end
-  
-  #Résolution
-  combinations = three_figures_combination
-  
-  # Affichage
-  puts combinations
+end
+
+compteur(0, 0, 0, 0)
